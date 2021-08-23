11e35bba4b78088b8ffa4b161f0b18b09e374617
TaoLoading <1377226380@qq.com>
2021-08-23 15:34:42 +0800

    项目框架搭建

---
+++ src/components/MapView/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>MapView</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top-view></top-view>
+    <sales-view></sales-view>
+    <map-view></map-view>
+    <bottom-view></bottom-view>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
+import MapView from '../components/MapView'
+import BottomView from '../components/BottomView'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TopView,
+    SalesView,
+    MapView,
+    BottomView
   }
 }
 </script>
+<style>
+.home {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: #eee;
+  box-sizing: border-box;
+}
+</style>
