c27c1a5dbeb39102531fbb9610982a21abe50cfd
TaoLoading <1377226380@qq.com>
2021-08-23 16:31:13 +0800

    项目框架搭建

---
+++ src/components/CommonCard/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="common-card">
+    <div class="title">1</div>
+    <div class="value">2</div>
+    <div class="chart">3</div>
+    <div class="line">4</div>
+    <div class="total">5</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonCard'
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: 12px;
+  color: #999;
+}
+.value {
+  font-size: 25px;
+  color: #000;
+  margin-top: 5px;
+  letter-spacing: 1px;
+}
+.chart {
+  height: 50px;
+}
+.line {
+  margin: 10px 0;
+  border-top: 1px solid #eee;
+}
+.total {
+  font-size: 12px;
+  color: #666;
+}
+</style>
---
+++ src/components/TodayUsers/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <common-card></common-card>
+</template>
+
+<script>
+import commonCardMixin from '../../mixins/commonCardMixin'
+export default {
+  mixins: [commonCardMixin]
+}
+</script>
+
+<style scoped>
+</style>
