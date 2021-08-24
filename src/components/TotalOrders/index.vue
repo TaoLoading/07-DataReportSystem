dbe4bd27eef499a8322c9e09f93749ba3841920a
TaoLoading <1377226380@qq.com>
2021-08-24 14:13:47 +0800

    使用vue-charts重构图表

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -2,9 +2,7 @@
   <common-card title="累计订单量"
                :value="666666">
     <template>
-      <!-- <v-chart :options="getOptions()" /> -->
-      <div id="total-orders-chart"
-           :style="{width:'100%',height:'100%'}"></div>
+      <v-chart :options="getOptions()" />
     </template>
     <template v-slot:footer>
       <span>昨日订单量 </span>
@@ -17,50 +15,13 @@
 import commonCardMixin from '../../mixins/commonCardMixin'
 export default {
   mixins: [commonCardMixin],
-  mounted() {
-    const chartDom = document.getElementById('total-orders-chart')
-    const chart = this.$echarts.init(chartDom)
-    chart.setOption({
-      xAxis: {
-        type: 'category',
-        show: false,
-        // 取消默认距x轴的距离
-        boundaryGap: false
-      },
-      yAxis: {
-        show: false
-      },
-      series: [{
-        type: 'line',
-        data: [620, 432, 220, 534, 709, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
-        // 显示面积
-        areaStyle: {
-          color: 'purple'
-        },
-        lineStyle: {
-          width: 0
-        },
-        itemStyle: {
-          opacity: 0
-        },
-        // 平滑显示
-        smooth: true
-      }],
-      // 左右边距置为0
-      grid: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-      }
-    })
-  }
-  /* methods: {
+  methods: {
     getOptions() {
-      return this.orderTrend.length > 0 ? {
+      return {
         xAxis: {
           type: 'category',
           show: false,
+          // 取消默认距x轴的距离
           boundaryGap: false
         },
         yAxis: {
@@ -68,7 +29,8 @@ export default {
         },
         series: [{
           type: 'line',
-          data: this.orderTrend,
+          data: [620, 432, 220, 534, 709, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+          // 显示面积
           areaStyle: {
             color: 'purple'
           },
@@ -78,17 +40,19 @@ export default {
           itemStyle: {
             opacity: 0
           },
+          // 平滑显示
           smooth: true
         }],
+        // 左右边距置为0
         grid: {
           top: 0,
           bottom: 0,
           left: 0,
           right: 0
         }
-      } : null
+      }
     }
-  } */
+  }
 }
 </script>
 
---
+++ src/plugins/vcharts.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VELine from 'v-charts/lib/line.common'
+
+Vue.component('ve-line', VELine)
