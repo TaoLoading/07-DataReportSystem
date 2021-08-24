bee893bbb4d46f572c01288db3f91f55850cc5ad
TaoLoading <1377226380@qq.com>
2021-08-24 15:37:24 +0800

    销售额展示模块

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,17 @@
+/**
+ * Element UI按需加载
+ */
 import Vue from 'vue'
-import { Card, Row, Col } from 'element-ui'
+import { Card, Row, Col, Menu, MenuItem, RadioGroup, RadioButton, DatePicker, Table, TableColumn, Pagination } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
--- src/plugins/vcharts.js
+++ src/plugins/vcharts.js
@@ -1,3 +1,6 @@
+/**
+ * v-charts按需加载
+ */
 import Vue from 'vue'
 import VELine from 'v-charts/lib/line.common'
 
