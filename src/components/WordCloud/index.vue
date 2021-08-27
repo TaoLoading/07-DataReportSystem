441281f6fbdde5dd1e32af0fe1e113d43a060652
TaoLoading <1377226380@qq.com>
2021-08-27 09:28:40 +0800

    词云图

--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>msg</div>
+  <ve-wordcloud :data="data"
+                height="100%"></ve-wordcloud>
 </template>
 
 <script>
@@ -7,6 +8,32 @@ export default {
   name: '',
   data() {
     return {
+      data: {
+        columns: ['name', 'value'],
+        rows: [
+          {
+            name: '土豆炖牛肉', value: 66
+          },
+          {
+            name: '肉丝面', value: 80
+          },
+          {
+            name: '黄焖鸡', value: 50
+          },
+          {
+            name: '甜点', value: 60
+          },
+          {
+            name: '水饺', value: 70
+          },
+          {
+            name: '烤鸭', value: 50
+          },
+          {
+            name: '奶茶', value: 100
+          }
+        ]
+      }
     }
   },
   methods: {
